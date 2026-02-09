ba85542e309dfbd1be3d9ab198031874168dafa7
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-09 20:45:16 +0100

    backend structure code commit by ENGR. IPAYE

--- backend/src/main/java/dev/engripaye/backend/controller/UserController.java
+++ backend/src/main/java/dev/engripaye/backend/controller/UserController.java
@@ -1,11 +1,44 @@
 package dev.engripaye.backend.controller;
 
+import dev.engripaye.backend.model.User;
+import dev.engripaye.backend.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/open-account")
+    public String openAccountPage(){
+        return "open-account";
+    }
+
+    @PostMapping("/open-account")
+    public String openAccount(@RequestParam String fullname,
+                              @RequestParam String email,
+                              HttpSession session){
+        User user = userService.register(fullname, email);
+        session.setAttribute("user", user);
+
+        return "redirect:/account/create";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/";
+    }
 
 }
