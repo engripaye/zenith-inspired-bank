5b25d4e3a9da0184a5605746453931758e0950b0
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-09 10:14:23 +0100

    backend structure code commit by ENGR. IPAYE

--- backend/src/main/java/dev/engripaye/backend/model/User.java
+++ backend/src/main/java/dev/engripaye/backend/model/User.java
@@ -1,38 +1,17 @@
 package dev.engripaye.backend.model;
 
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
 
     private String fullname;
     private String email;
     private String balance;
 
-    public User(String fullname, String email, String balance) {
-        this.fullname = fullname;
-        this.email = email;
-        this.balance = balance;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getBalance() {
-        return balance;
-    }
-
-    public void setBalance(String balance) {
-        this.balance = balance;
-    }
 }
