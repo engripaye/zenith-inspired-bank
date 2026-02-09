c38dec8fba06abd1e00dbd919d299ef2702ba3c0
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-09 12:46:23 +0100

    backend structure code commit by ENGR. IPAYE

---
+++ backend/src/main/java/dev/engripaye/backend/controller/UserController.java
@@ -0,0 +1,11 @@
+package dev.engripaye.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+
+}
