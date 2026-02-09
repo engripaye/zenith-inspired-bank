f7373fc4d5948760efadc6137ad1b281620f59b3
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-09 11:02:25 +0100

    backend structure code commit by ENGR. IPAYE

---
+++ backend/src/main/java/dev/engripaye/backend/service/UserService.java
@@ -0,0 +1,17 @@
+package dev.engripaye.backend.service;
+
+import dev.engripaye.backend.model.User;
+import dev.engripaye.backend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User register(String fullname, String email){
+        return userRepository.save(new User(null, fullname, email, "1,200,200"));
+    }
+}
