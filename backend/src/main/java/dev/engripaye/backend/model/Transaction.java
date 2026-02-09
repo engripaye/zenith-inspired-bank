289898647a84854320f1776808368e3ec249d5ac
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-09 10:30:23 +0100

    backend structure code commit by ENGR. IPAYE

--- backend/src/main/java/dev/engripaye/backend/model/Transaction.java
+++ backend/src/main/java/dev/engripaye/backend/model/Transaction.java
@@ -1,4 +1,31 @@
 package dev.engripaye.backend.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "transactions")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private double amount;
+
+    private LocalDateTime transactionDate;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
 }
