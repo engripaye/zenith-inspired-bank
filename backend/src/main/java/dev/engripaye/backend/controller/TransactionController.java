cb2e8585f8c6d1c14aad80ce9ff566327884862c
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-09 21:22:13 +0100

    backend structure code commit by ENGR. IPAYE

---
+++ backend/src/main/java/dev/engripaye/backend/controller/TransactionController.java
@@ -0,0 +1,50 @@
+package dev.engripaye.backend.controller;
+
+
+import dev.engripaye.backend.model.Account;
+import dev.engripaye.backend.service.TransactionService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping("/deposit")
+    public String deposit(@RequestParam BigDecimal amount, HttpSession httpSession){
+
+        Account account = (Account) httpSession.getAttribute("account");
+        transactionService.deposit(account, amount);
+
+        return "redirect:/account/dashboard";
+
+    }
+
+    @PostMapping("/withdraw")
+    public String withdraw(@RequestParam BigDecimal amount, HttpSession httpSession){
+        Account account = (Account) httpSession.getAttribute("account");
+        transactionService.withdraw(account, amount);
+
+        return "redirect:/account/dashboard";
+    }
+
+    @PostMapping("/transfer")
+    public String transfer(@RequestParam BigDecimal amount,
+                           @RequestParam Account receiver,
+                           HttpSession session){
+        Account sender = (Account) session.getAttribute("account");
+        transactionService.transfer(sender, receiver, amount);
+
+        return "redirect:/account/dashboard";
+    }
+
+}
